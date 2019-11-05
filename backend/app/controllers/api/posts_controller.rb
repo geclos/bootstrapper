b63e4ae2d676cdc18ebbb63a5c4e7de156491d08
Gerard Clos <clos.gerard@gmail.com>
2019-11-05 09:35:47 +0100

    fix some hilillos de plastelina

--- backend/app/controllers/api/posts_controller.rb
+++ backend/app/controllers/api/posts_controller.rb
@@ -1,24 +1,26 @@
 require 'rss'
 require 'open-uri'
 
-class PostsController < ApplicationController
-  def index
-    url = 'http://feeds.feedburner.com/marginalrevolution/feed'
+module Api
+  class PostsController < ApplicationController
+    def index
+      url = 'http://feeds.feedburner.com/marginalrevolution/feed'
 
-    items = open(url) do |rss|
-      feed = RSS::Parser.parse(rss)
-      feed.items.each_with_index.map do |item, i|
-        {
-          id: i,
-          title: item.title,
-          content: item.content_encoded,
-          description: item.description,
-          link: item.link,
-          pub_date: item.pubDate
-        }
+      items = open(url) do |rss|
+        feed = RSS::Parser.parse(rss)
+        feed.items.each_with_index.map do |item, i|
+          {
+            id: i,
+            title: item.title,
+            content: item.content_encoded,
+            description: item.description,
+            link: item.link,
+            pub_date: item.pubDate
+          }
+        end
       end
-    end
 
-    render json: items
+      render json: items
+    end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     root to: 'home#index'
   end
 
-  scope module: 'public' do
+  scope module: 'api' do
     resources :posts
   end
 end
